fd45c45ec8ba56328b0087a642f5ddb9ef4ab3a1
Olu Fadipe <2fadio51@solent.ac.uk>
2018-11-12 12:38:25 +0000

    Created a form so users can add their Point of Interest

---
+++ addpoi2.php
@@ -0,0 +1,47 @@
+<?php
+ 
+  ?>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Point of Interest</title>
+</head>
+
+<body>
+   
+    <main>
+        <h2>
+            Solent Restaurants
+        </h2>
+        <form action="xxx.php" method="POST">
+            <br> Name:
+            <br>
+            <input type="text" name="name">
+            <br> Address:
+            <br>
+            <input type="text" name="address">
+            <br> Country:
+            <br>
+            <input type="text" name="country">
+            <br> Region:
+            <br>
+            <input type="text" name="region">
+            <br> Lat:
+            <br>
+            <input type="text" name="lat">
+            <br> Lon:
+            <br>
+            <input type="text" name="Lon">
+            
+            <br>
+
+
+            <input type="submit" value="Add Point of Interest">
+        </form>
+    </main>
+
+
+</body>
+
+</html>
