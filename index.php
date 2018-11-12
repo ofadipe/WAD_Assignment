a7a63fe1ad003bdbe3db85498508d6c663e4140c
Olu Fadipe <2fadio51@solent.ac.uk>
2018-11-12 12:37:50 +0000

    Added link to form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Point of Interest </title>
+</head>
+<body>
+    <h1>POI Web Service</h1>
+<form method="post" action="poisearchservice.php">
+
+ <p>
+      <a href='addpoi2.php'> Add New Point of Interest</a>
+</p>
+
+
+</form>
+</body>
+</html>
